f90dbd4a2d937d14c7c3ad661f2c512cfa51aa30
Омаров Багаутдин <b.omarov@crpt.ru>
2023-04-27 05:27:06 +0300

    auth

--- src/config.ts
+++ src/config.ts
@@ -10,8 +10,8 @@ export default {
     scheme: "https",
   },
   oauth: {
-    CLIENT_ID: "23cabbbdc6cd418abb4b39c32c41195d",
-    CLIENT_SECRET: "53bc75238f0c4d08a118e51fe9203300",
+    CLIENT_ID: "f7c276c60c724cca99b42466e629a742",
+    CLIENT_SECRET: "236b79fedb7e429c838ce11ac1668bd4",
   },
   fake_device: {
     DEVICE_ID: "377c5ae26b09fccd72deae0a95425559",
