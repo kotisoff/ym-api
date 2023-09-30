e18f73e2fc6db7e2e9fe26bace4d03838880d6d2
Котя <71443736+kotisoff@users.noreply.github.com>
2023-09-30 14:33:13 +0300

    getTrackShareLink<any> to <string>

--- src/YMApi.ts
+++ src/YMApi.ts
@@ -537,7 +537,7 @@ export default class YMApi {
   /**
    * @returns track sharing link
    */
-  async getTrackShareLink(track: TrackId | Track): Promise<any> {
+  async getTrackShareLink(track: TrackId | Track): Promise<string> {
     let albumid = 0,
       trackid = 0;
     if (typeof track === "object") {
